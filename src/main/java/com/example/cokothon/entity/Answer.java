47fa2433cf942cd1dec9c30bb7d06bd86c6197f6
JINU-MAN <wlsdnjinu@gmail.com>
2025-08-30 22:06:37 +0900

    db

---
+++ src/main/java/com/example/cokothon/entity/Answer.java
@@ -0,0 +1,29 @@
+package com.example.cokothon.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.JoinColumn;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.example.cokothon.common.entity.BaseEntity;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "answers")
+public class Answer extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "test_id", foreignKey = @ForeignKey(name = "fk_answer_test"))
+    private Question problem;
+    
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;  // answer 내용
+}
---
+++ src/main/java/com/example/cokothon/entity/UserAnswerId.java
@@ -0,0 +1,5 @@
+package com.example.cokothon.common.entity;
+
+public class UserAnswerId {
+
+}
